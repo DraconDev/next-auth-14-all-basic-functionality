f518a56a84407e14501b6e8f9bf9cf7d6fe28970
DraconDev <dracsharp@gmail.com>
2023-11-03 14:39:09 +0000

    del extra code

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
-    const session = await getServerSession();
     return (
         <main className="flex min-h-screen flex-col items-center p-24 gap-2">
             <Link href="/api/auth/signin" className="text-3xl p-1 bg-slate-600">
@@ -12,7 +11,7 @@ export default async function Home() {
                 Sign in
             </Link>
             <Link href="/checkIfLogged" className="text-3xl p-1 bg-slate-600">
-            checkIfLogged
+                checkIfLogged
             </Link>
             <Link
                 href="/showLoginIfNotLogged"
