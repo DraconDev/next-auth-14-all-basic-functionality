db08715a9619336836f648c4de02c0b6315f19c1
DraconDev <dracsharp@gmail.com>
2023-11-30 15:48:38 +0000

    upg loginlogo

--- components/Auth/LoginLogo.tsx
+++ components/Auth/LoginLogo.tsx
@@ -1,17 +1,20 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { FaUserAltSlash } from "react-icons/fa";
+import { FaUserPlus } from "react-icons/fa";
 import { TbUserHexagon } from "react-icons/tb";
-
 export default function LoginLogo() {
     const { data: session } = useSession();
     return (
         <>
             <button
                 onClick={() => (session ? signOut() : signIn())}
-                className="bg-secondary hover:bg-accent text-white p-3 rounded-lg shadow-lg text-xl "
+                className="bg-secondary hover:bg-accent text-white p-2 rounded-lg shadow-lg"
             >
-                {session ? <TbUserHexagon /> : <FaUserAltSlash />}
+                {session ? (
+                    <TbUserHexagon className="h-7 w-7" />
+                ) : (
+                    <FaUserPlus className="h-7 w-7" />
+                )}
             </button>
         </>
     );
