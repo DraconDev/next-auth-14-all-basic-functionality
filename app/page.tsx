ee46ab805f6900cc2ab91acd3f0af12e50fa81f6
DraconDev <dracsharp@gmail.com>
2023-11-30 15:27:25 +0000

    upd main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,18 @@ import Link from "next/link";
 export default async function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center p-24 gap-2">
-            <div className="text-3xl">Test Routes</div>
+            <div className="text-3xl w-full border-b-2 border-red-700 border border-0 flex justify-center">
+                Test Routes
+            </div>
             <Link
                 href="/checkIfLogged"
                 className="text-3xl p-1 bg-slate-600"
             >
                 checkIfLogged
             </Link>
-            <div className="text-3xl">Test Components</div>
-            {/* <Link
-                href="/api/auth/signin"
-                className="text-3xl p-1 bg-slate-600"
-            >
-                {" "}
-                Sign in
-            </Link> */}
-
+            <div className="text-3xl w-full border-b-2 border-red-700 border border-0 flex justify-center">
+                Test Components
+            </div>
             <Link
                 href="/showLoginIfNotLogged"
                 className="text-3xl p-1 bg-slate-600"
